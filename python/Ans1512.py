6040b1464efa85f2a225ee8e87c34759165eadfe
Aryan_Verma <138862928+thevermas@users.noreply.github.com>
2023-10-03 10:35:44 +0530

    Ans1512.py
    New Answer for question 1512 Number of good pairs

---
+++ python/Ans1512.py
@@ -0,0 +1,22 @@
+# LeetCode Solution for 1512_Number_Of_Good_Pairs
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        # Create a dictionary to count the occurrences of each number
+        num_count = {}
+
+        # Initialize the count of good pairs
+        good_pairs = 0
+
+        # Iterate through the array and count occurrences
+        for num in nums:
+            if num in num_count:
+                num_count[num] += 1
+            else:
+                num_count[num] = 1
+
+        # Calculate good pairs for each number that occurs more than once
+        for count in num_count.values():
+            if count > 1:
+                good_pairs += (count - 1) * count // 2
+
+        return good_pairs
